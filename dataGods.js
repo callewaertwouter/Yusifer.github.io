c1d3e19fc7f96157bbf06aa39d68fc22ba624982
CallewaertWouter <44768101+Yusifer@users.noreply.github.com>
2021-08-27 10:11:58 +0200

    Add files via upload

---
+++ dataGods.js
@@ -0,0 +1,273 @@
+const dataGods = `{
+    "Europe": {
+        "Greek": {
+            "info": "The number of Greek Gods is huge since the ancient Greeks believed in many deities and spirits. Because knowledge of the world was limited in antiquity, the Ancients were attributing natural phenomena to higher powers. Gods and goddesses could be found everywhere and defined the lives of the people. The twelve gods of Olympus are the most famous ones and were indeed at the center of the ancient Greek religion. However, there were many more than the twelve, and many of them were rulers of the cosmos before the well-known Olympians. Let’s start from the beginning and break down the chaotic family tree of the Greek Gods!",
+            "sanctuaries": [
+                "Delphi",
+                "Athens",
+                "Olympia"
+            ],
+            "gods": [
+                {
+                    "name": "Zeus",
+                    "demi-god": false,
+                    "description": "God of the sky, lightning, thunder, law, order, justice, King of the Gods and the 'Father of Gods and men'.",
+                    "image": "zeus.jpg",
+                    "friends": [
+                        "Hera",
+                        "Kronos",
+                        "Poseidon"
+                    ]
+                },
+                {
+                    "name": "Poseidon",
+                    "demi-god": false,
+                    "description": "Olympian Greek god of the sea, earthquakes, storms, and horses.",
+                    "image": "poseidon.jpg",
+                    "friends": [
+                        "Hades",
+                        "Eros",
+                        "Cronus",
+                        "Zephyrus",
+                        "Pollux"
+                    ]
+                },
+                {
+                    "name": "Hermes",
+                    "demi-god": false,
+                    "description": "God of trade, thieves, travelers, sports, athletes, and border crossings, guide to the Underworld and messenger of the gods.",
+                    "image": "hermes.jpg",
+                    "friends": "Zephyrus"
+                },
+                {
+                    "name": "Aeneas",
+                    "demi-god": true,
+                    "description": "Along with his cousin Hector, he played a prominent role in the battle of Troy. According to some sources, he was made the leader of the Trojan survivors after the war.",
+                    "image": "aeneas.jpg",
+                    "friends": [
+                        "Aphrodite",
+                        "Anchises"
+                    ]
+                }
+
+            ]
+        },
+        "Norse": {
+            "info": "Norse mythology is the body of myths of the North Germanic peoples, stemming from Norse paganism and continuing after the Christianization of Scandinavia, and into the Scandinavian folklore of the modern period. The northernmost extension of Germanic mythology, Norse mythology consists of tales of various deities, beings, and heroes derived from numerous sources from both before and after the pagan period, including medieval manuscripts, archaeological representations, and folk tradition.",
+            "sanctuaries": [
+                "Asgard",
+                "Bifröst",
+                "Jotunheim",
+                "Hlidskjalf",
+                "Hvergelmir"
+
+            ],
+            "gods": [
+                {
+                    "name": "Loki",
+                    "demi-god": false,
+                    "description": "The sly, trickster of the Norse gods. Son of two giants. Also known as the Sly One, the Trickster, the Shape Changer and the Sky Traveller. Becomes increasingly more evil. He is responsible for the death of Balder. Bound until Ragnarok.",
+                    "image": "Loki.jpg",
+                    "friends": [
+                        "Thor",
+                        "Odin",
+                        "Heimdall",
+                        "Harbard",
+                        "Gullveig",
+                        "Hel",
+                        "Njord"
+                    ]
+                },
+                {
+                    "name": "Odin",
+                    "demi-god": false,
+                    "description": "King of the Norse Gods, God of poetry, battle and death. Chief god of the Aesir. Also known as the “all-father”, the “terrible one”, “one-eyed” and “father of battle”",
+                    "image": "odin.jpg",
+                    "friends": "Aegir"
+                },
+                {
+                    "name": "Thor",
+                    "demi-god": false,
+                    "description": "God of Sky, thunder and fertility. Associated with law and order in Asgard and guardian of the Norse gods. Son of Odin and Earth and husband of Sif. Also known as the “thunder god” and “charioteer”",
+                    "image": "thor.jpg",
+                    "friends": [
+                        "Nanna",
+                        "Magni",
+                        "Valkyries"
+                    ]
+                },
+                {
+                    "name": "Heimdall",
+                    "demi-god": false,
+                    "description": "Watchman of the Norse gods and owner of the horn Gjall. Son of nine mothers. Often identified with Rig, the creator of three races of men.",
+                    "image": "heimdall.jpg",
+                    "friends": [
+                        "Loki",
+                        "Idun"
+                    ]
+                }
+            ]
+        },
+        "Celtic": {
+            "info": "Celtic is the mythology of Celtic polytheism, the religion of the Iron Age Celts.[1] Like other Iron Age Europeans, the early Celts maintained a polytheistic mythology and religious structure. For Celts in close contact with Ancient Rome, such as the Gauls and Celtiberians, their mythology did not survive the Roman Empire, their subsequent conversion to Christianity and the loss of their Celtic languages. It is mostly through contemporary Roman and Christian sources that their mythology has been preserved. The Celtic peoples who maintained either political or linguistic identities (such as the Gaels in Ireland and Scotland, the Welsh in Wales, and the Celtic Britons of southern Great Britain and Brittany) left vestigial remnants of their ancestral mythologies that were put into written form during the Middle Ages.",
+            "sanctuaries": [
+                "Stonehenge",
+                "Hardriens Wall",
+                "Loch Ness",
+                "Carnac"
+            ],
+            "gods": [
+                {
+                    "name": "Dagda",
+                    "demi-god": false,
+                    "description": "The Dagda was chief of the Irish Tuatha dé Danann, god of life and death, agriculture and fertility. A learned advisor and master of druidic magic, he possessed a cauldron that provided plenty, a massive club that granted life or death, and an enchanted harp that ordered the seasons.",
+                    "image": "dagda.jpg",
+                    "friends": [
+                        "Danu",
+                        "Cernunnos",
+                        "Gwydion"
+                    ]
+                },
+                {
+                    "name": "Morrigan",
+                    "demi-god": false,
+                    "description": "The fearful Phantom Queen known as the Morrígan was the Irish goddess of death, discord, and war. She was portrayed both as a single entity and as three sisters who could issue terrifying prophecy and foretell the deaths of warriors and kings.",
+                    "image": "morrigan.jpg",
+                    "friends": "Badb"
+                },
+                {
+                    "name": "Lugh",
+                    "demi-god": false,
+                    "description": "Lugh of the Long Arm was a young Irish god who sought revenge for his father’s unjust death. A master of all skills, he lead the Tuatha dé Danann to victory against the Fomorians. His death marked the decline of his tribe’s presence in Ireland.",
+                    "image": "lugh.jpg",
+                    "friends": [
+                        "Herne the Hunter",
+                        "Taranis",
+                        "Taliesin",
+                        "Neit"
+                    ]
+                },
+                {
+                    "name": "Cu Chulainn",
+                    "demi-god": false,
+                    "description": "The great hero of the Ulster Cycle, Cú Chulainn was a man of great passions whose skills were rarely matched in battle. A proud warrior, he could fight back hordes of enemies with unmatched rage.",
+                    "image": "cuchulainn.jpg",
+                    "friends": [
+                        "Brigid",
+                        "Aengus",
+                        "Cernunnos",
+                        "Morrigan"
+                    ]
+                }
+            ]
+        }
+    },
+    "Asia": {
+        "Hindu": {
+            "info": "Hinduism is the largest religion in the Indian subcontinent and third largest religion in the world. It comprises five major sects or denominations, Vaishnavism, Shaivism, Shaktism, Ganapatism,[1] and Saurism whose followers consider Vishnu, Shiva, Shakti (Devi), Ganesha, and Surya to be the Supreme deity respectively. Smartism sect considers all the above five deities as equal. Most of the other deities were either related to them or different forms (incarnations) of these deities. Hinduism has been called the 'oldest religion' in the world, and many practitioners refer to Hinduism as 'the eternal law'.",
+            "sanctuaries": [
+                "Jaganathapuri temple",
+                "Badrinath",
+                "Dwarka"
+            ],
+            "gods": [
+                {
+                    "name": "Brahma",
+                    "demi-god": false,
+                    "description": "According to Hinduism, Brahma is the creator of the entire cosmic universe. Although he is the creator, he is hardly worshipped in modern Hinduism. He is identified with supreme vedic god, Prajapati. He married Saraswati, who emerged to give knowledge to create.",
+                    "image": "brahma.jpg",
+                    "friends": [
+                        "Devi",
+                        "Shiva",
+                        "Vishnu",
+                        "Dashavatara"
+                    ]
+                },
+                {
+                    "name": "Devi",
+                    "demi-god": false,
+                    "description": "Communities of goddess worship are ancient in India. In the Rigveda, the most prominent goddess is Ushas, the goddess of dawn. In modern Hinduism, goddesses are widely revered. Shaktism is one of the major sects of Hinduism. Followers of Shaktism believe that the goddess (Devi) is the power (Shakti) that underlies the female principle, and that Devi is the supreme being, one and the same with Para Brahman. Shakti has many forms/manifestations like Parvati, Durga, and others but there are also goddesses that are parts of Shakti such as Lakshmi and Saraswati. Devi is believed to manifest in peaceful forms, such as Parvati the consort of Shiva and also in fierce forms, such as Kali and Durga. In Shaktism, Adi Parashakti is regarded as Ultimate Godhead or Para Brahman. She is formless i.e. Nirguna in reality, but may take many forms i.e. Saguna. Durga and Lalita Tripurasundari are regarded as the Supreme goddess in the Kalikula and Srikula systems respectively. Shaktism is closely related with Tantric Hinduism, which teaches rituals and practices for purification of the mind and body.",
+                    "image": "devi.jpg",
+                    "friends": [
+                        "Rudras",
+                        "Adityas",
+                        "Ashvins"
+                    ]
+                },
+                {
+                    "name": "Shiva",
+                    "demi-god": false,
+                    "description": "Shaivism is one of the major Hindu sects. Adherents of Shaivism believe that the god Shiva is the supreme being. Shiva is the destroyer god among the Trimurti, and so is sometimes depicted as the fierce god Bhairava. Shaivists are more attracted to asceticism than adherents of other Hindu sects, and may be found wandering with ashen faces performing self-purification rituals.",
+                    "image": "shiva.jpg",
+                    "friends": "Ganesha"
+                },
+                {
+                    "name": "Vishnu",
+                    "demi-god": false,
+                    "description": "Vaishnavism is the sect within Hinduism that worships Vishnu, the preserver god of the Hindu Trimurti (the Trinity), and his many incarnations. Vaishnavites regard him to be eternal and the strongest and supreme God . It is a devotional sect, and followers worship many deities, including Rama and Krishna both the 7th & the 8th incarnations of Vishnu respectively. The adherents of this sect are generally non-ascetic, monastic and devoted to meditative practice and ecstatic chanting.",
+                    "image": "vishnu.jpg",
+                    "friends": [
+                        "Ananta Shayana",
+                        "Hari",
+                        "Govinda"
+                    ]
+                }
+            ]
+        }
+    },
+    "Africa": {
+        "Egyptian": {
+            "info": "Egyptian mythology is the collection of myths from ancient Egypt, which describe the actions of the Egyptian gods as a means of understanding the world around them. The beliefs that these myths express are an important part of ancient Egyptian religion. Myths appear frequently in Egyptian writings and art, particularly in short stories and in religious material such as hymns, ritual texts, funerary texts, and temple decoration. These sources rarely contain a complete account of a myth and often describe only brief fragments. Inspired by the cycles of nature, the Egyptians saw time in the present as a series of recurring patterns, whereas the earliest periods of time were linear. Myths are set in these earliest times, and myth sets the pattern for the cycles of the present. Present events repeat the events of myth, and in doing so renew maat, the fundamental order of the universe.",
+            "sanctuaries": [
+                "Caïro",
+                "Abu Simbel",
+                "Abu al-Abbas al-Mursi"
+            ],
+            "gods": [
+                {
+                    "name": "Horus",
+                    "demi-god": false,
+                    "description": "Horus was the Egyptian god of the Sky. He was the son of Isis and Osiris. After fighting his uncle, Set (Osiris and Isis' brother) he became the king of Egypt. During this fight, he lost his eye. The Eye of Horus became one of the most important symbols in ancient Egypt and Egyptian civilization. When Horus lost his eye, his eye set out on a journey of its own. When the other Gods tried to catch the eye, his eye began to cry. These tears became the first people of Egypt in Egyptian belief.",
+                    "image": "horus.jpg",
+                    "friends": [
+                        "Ptah",
+                        "Montu",
+                        "Osiris",
+                        "Neper",
+                        "Bennu"
+                    ]
+                },
+                {
+                    "name": "Ra",
+                    "demi-god": false,
+                    "description": "In Egyptian mythology, Ra was the god of the sun. He was the most important god in Ancient Egypt. He had many names, such as Amun-Ra, and Ra-Horakhty. It was said he was born each morning in the East, and died each night in the West. In the night he travelled through the underworld. This is why the west side of the Nile was known as the land of the dead. He had the head of a Falcon, the body of a man, and was the king of the Egyptian gods.",
+                    "image": "ra.jpg",
+                    "friends": [
+                        "Tatenen",
+                        "Kek",
+                        "Amunet",
+                        "Horus"
+                    ]
+                },
+                {
+                    "name": "Anubus",
+                    "demi-god": false,
+                    "description": "Anubis is the protector of the gates to the Underworld, Osiris replaced him as the god of the dead. He looks like a man with the head of a jackal (an animal, native to Africa, similar to wolves).",
+                    "image": "anubis.jpg",
+                    "friends": [
+                        "Horus",
+                        "Wosret",
+                        "Ra"
+                    ]
+                },
+                {
+                    "name": "Set",
+                    "demi-god": false,
+                    "description": "In Egyptian mythology, Set' (also spelled Sutekh, Setesh, Seteh, Seth) was a god of the desert (including desert storms), thunder, evil, pain and suffering.",
+                    "image": "set.png",
+                    "friends": "Aker"
+                }
+            ]
+        }
+    }
+}`;
