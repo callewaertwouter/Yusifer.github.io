b431cebe521bd0a29caf97882051d24fe072a008
CallewaertWouter <44768101+callewaertwouter@users.noreply.github.com>
2022-08-25 13:19:13 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,107 @@
+const games = `{
+  "PS4": {
+    "logo": "ps4.png",
+    "games": [{
+        "id": 1,
+        "name": "God of War",
+        "price": 69.99,
+        "type": "Action",
+        "description": "Nadat hij wraak heeft genomen op de goden van Olympus, trekt Kratos naar het universum van de noordse goden en monsters.",
+        "image": "gow.jpg",
+        "detailImages": ["gow1.jpg", "gow2.jpg"]
+      },
+      {
+        "id": 2,
+        "name": "Monster Hunter World",
+        "price": 59.99,
+        "type": "Adventure",
+        "description": "Ga op expeditie en verken een pas ontdekt, levensgevaarlijk continent dat bekendstaat als 'De Nieuwe Wereld'. Jaag in de onbekende wildernis van Monster Hunter: World op de meest afschrikwekkende beesten.",
+        "image": "mh.jpg",
+        "detailImages": ["mh1.jpg", "mh2.jpg"]
+
+      },
+      {
+        "id": 3,
+        "name": "Grand Theft Auto",
+        "price": 73.99,
+        "type": "Adventure",
+        "description": "Maak kennis met drie heel verschillende criminelen, Michael, Franklin en Trevor, en maak mee hoe ze alles riskeren in een reeks gedurfde en gevaarlijke overvallen die hen schatrijk kunnen maken.",
+        "image": "gta.jpg",
+        "detailImages": ["gtav.jpg", "gtav2.jpg"]
+      },
+      {
+        "id": 4,
+        "name": "Minecraft",
+        "price": 29.99,
+        "type": "Adventure",
+        "description": "In Minecraft: PlayStation 4 Edition begint het avontuur met je eigen verbeelding. Je kunt blokken breken, plaatsen en ermee doen wat je wilt. Probeer een zo diep mogelijk gat te graven of maak een schuilplaats om 's nachts in te schuilen voor de verschrikkelijke monsters. Van stenen hut tot kasteel, van eenvoudig idee tot super-ambitieus plan; jij bepaalt hoe het spel wordt gespeeld.",
+        "image": "mc.jpg",
+        "detailImages": ["mc1.jpg", "mc2.jpg"]
+      },
+      {
+        "id": 5,
+        "name": "Call of Duty",
+        "price": 79.99,
+        "type": "Shooter",
+        "description": "Keer terug naar de dodelijkste oorlog van de 20e eeuw en ervaar de dramatische achtergrond die de inspiratie vormde voor de immens populaire Call of Duty-serie, nu opnieuw vormgegeven voor een nieuwe generatie gamers.",
+        "image": "cod.jpg",
+        "detailImages": ["cod1.jpg", "cod2.jpg"]
+      }
+    ]
+  },
+  "Xbox-One": {
+    "logo": "xbox-One.png",
+    "games": [{
+        "id": 6,
+        "name": "Sea of Thieves",
+        "price": 79.99,
+        "type": "Adventure",
+        "description": "Sea of Thieves is een nieuw type  multiplayer game die je alles geeft wat je nodig hebt om te leven als een piraat. Ofwel een avontuur als een groep, of eenzame vaarder zul je andere mensen ontmoeten... zijn ze vriend of vijand en hoe zul jij dit behandelen ... ",
+        "image": "sot.jpg",
+        "detailImages": ["sot1.jpg", "sot2.jpg"]
+      },
+      {
+        "id": 7,
+        "name": "Destiny 2",
+        "price": 49.99,
+        "type": "Shooter",
+        "description": "De laatste vrije stad van de mensen is gevallen. De brute commandant van het Red Legion, Ghaul, heeft een overweldigende legermacht naar de overwinning gevoerd en de Guardians van de stad van hun macht beroofd.",
+        "image": "destiny.jpg",
+        "detailImages": ["destiny1.jpg", "destiny2.jpg"]
+      },
+      {
+        "id": 8,
+        "name": "Far Cry 5",
+        "price": 69.99,
+        "type": "Adventure",
+        "description": "Gevangen achter de vijandelijke linies van een secte.Wanneer het Project bij Eden's Gate — een bende fanatieke sekteleden die vastbesloten is om zielen te redden, of mensen dat nu willen of niet — hun bolwerk in Hope County, Montana in rap tempo versterkt, hebben de in het nauw gedreven bewoners twee keuzes: de redding van de sekte accepteren of in de tegenaanval gaan. ",
+        "image": "fcx.jpg",
+        "detailImages": ["fc1.jpg", "fc2.jpg", "fc3.jpg"]
+      }
+
+    ]
+  },
+  "3DS": {
+    "logo": "3ds.png",
+    "games":
+      [{
+          "id": 9,
+          "name": "Batman",
+          "price": 39.99,
+          "type": "Action",
+          "description": " Bouwen als Batman! Speel volgens je eigen regels met de coole voertuigen, gebouwen, personages en wapens uit DE LEGO® BATMAN FILM. De Joker™, Catwoman™, de Penguin™ en andere gekke schurken maken Gotham City onveilig. Help Batman die boeven achter de tralies te krijgen. Speel je favoriete scènes uit de film na of bedenk je eigen duels!",
+          "image": "bat.jpg",
+          "detailImages": ["bat1.jpg", "bat2.jpg"]
+        },
+        {
+          "id": 10,
+          "name": "Nintendogs",
+          "price": 39.99,
+          "type": "Soft Skills",
+          "description": "Het doel van het spel is om een puppy zo goed mogelijk af te richten waardoor hij op hondenshows zo goed mogelijk kan presteren. Daarmee kan een speler geld verdienen en daarvan dingen gaan kopen.",
+          "image": "dogs.jpg",
+          "detailImages": "dogs2.jpg"
+        }
+      ]
+  }
+}`;
